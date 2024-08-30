a48fdbc3a197350c1aaee2813aa7763a21ba8f3c
lmletham <lydia@csail.mit.edu>
2024-08-29 20:27:18 -0600

    add app.ex

---
+++ lib/startupcell.ex
@@ -0,0 +1,7 @@
+Mix.install([
+  {:kino, "~> 0.13.2"},
+  {:earmark, "~> 1.4.47"},
+  {:makeup, "~> 1.1"},
+  {:makeup_elixir, "~> 0.7"},
+  {:HWsmartcell, github: "lmletham/HWsmartcell", override: true}
+])
