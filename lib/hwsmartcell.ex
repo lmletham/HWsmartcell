2bbea2fd9e7adfc6d2aec38e77049ecd07424d91
lmletham <lydia@csail.mit.edu>
2024-09-03 18:43:46 -0600

    revert to _ assign Test Code

--- lib/hwsmartcell.ex
+++ lib/hwsmartcell.ex
@@ -100,7 +100,7 @@ defmodule Hwsmartcell do
     _ = ~s#{inspect(attrs["correct_answer"], raw: true)}
 
     #Test Code:
-    _ = ~s#{attrs["test_code"]}
+    _ = #{attrs["test_code"]}
     """
   end
 
