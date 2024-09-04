7674cdd7ed86ad9f79b071405a0af24e59b3e1a6
lmletham <lydia@csail.mit.edu>
2024-09-03 18:33:50 -0600

    try anon function

--- lib/hwsmartcell.ex
+++ lib/hwsmartcell.ex
@@ -100,7 +100,7 @@ defmodule Hwsmartcell do
     _ = ~s#{inspect(attrs["correct_answer"], raw: true)}
 
     #Test Code:
-    _ = Code.eval_string("#{attrs["test_code"]}")
+    _ = fn -> #{attrs["test_code"]} end.()
     """
   end
 
