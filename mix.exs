e3af3c2ff150296b26ea1f1329fe9c44840cb3e0
lmletham <lydia@csail.mit.edu>
2024-09-12 12:32:21 -0600

    update hex.pm version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hwsmartcell.MixProject do
   def project do
     [
       app: :hwsmartcell,
-      version: "0.1.23",
+      version: "0.1.24",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
